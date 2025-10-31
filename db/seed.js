d2213079adc6a93ede4c5d51c8534881b522bceb
Brixtongrey <brixtonaruda@gmail.com>
2025-10-30 19:47:06 -0600

    feat: seed.js seed function

--- db/seed.js
+++ db/seed.js
@@ -7,4 +7,36 @@ console.log("🌱 Database seeded.");
 
 async function seed() {
   // TODO
+  try {
+    console.log("Dropping tables...")
+    await db.query(`
+      DROP TABLE IF EXISTS playlists_tracks;
+      DROP TABLE IF EXISTS playlists;
+      DROP TABLE IF EXISTS tracks;
+      `);
+
+      console.log("Creating tables...");
+      await db.query(`
+        CREATE TABLE playlists (
+          id SERIAL PRIMARY KEY,
+          name TEXT NOT NULL,
+          description TEXT NOT NULL
+        );
+
+        CREATE TABLE tracks (
+          id SERIAL PRIMARY KEY,
+          name TEXT NOT NULL,
+          duration_ms INTEGER NOT NULL
+        );
+
+        CREATE TABLE playlists_tracks (
+          id SERIAL PRIMARY KEY,
+          playlist_id INTEGER NOT NULL playlists(id) ON DELETE CASCADE,
+          track_id INTEGER NOT NULL tracks(id) ON DELETE CASCADE,
+          CONSTRAINT unique_playlist_track UNIQUE (playlist_id, track_id)
+        );
+        `);
+  } catch (error) {
+
+  }
 }
